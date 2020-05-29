f2129f67586869502db3f424ea33d53597d8169d
Harry Peach <harrypeach1999@gmail.com>
2020-05-29 19:42:01 +0100

    Edit node testing action

---
+++ components/messages/Message.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import Message from "./Message";
+import { CardContent } from "@material-ui/core";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it("Renders timestamps correctly", () => {
+	const wrapper = shallow(<Message timestamp="1590681646606000000" />);
+	expect(wrapper.text()).toBe("Thu, 28 May 2020 16:00:46 GMT");
+});
+
+it("Can render without timestamp", () => {
+	const wrapper = shallow(<Message text="Hey there!" />);
+	expect(wrapper.text()).toBe("Hey there!");
+});
+
+it("Right aligns when provided the prop", () => {
+	const wrapperRight = shallow(<Message right />);
+	expect(wrapperRight.find(CardContent).get(0).props.className).toBe(
+		"rightAlign"
+	);
+
+	const wrapperLeft = shallow(<Message />);
+	expect(wrapperLeft.find(CardContent).get(0).props.className).not.toBe(
+		"rightAlign"
+	);
+});
---
+++ components/messages/TextBox.test.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import TextBox from "./TextBox";
+import { InputBase, IconButton } from "@material-ui/core";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it("Triggers action function when enter is pressed", () => {
+	const actionFunc = jest.fn();
+	const wrapper = shallow(
+		<TextBox
+			onChange={() => {
+				return;
+			}}
+			textbox="textboxValue"
+			sendAction={actionFunc}
+		/>
+	);
+	const input = wrapper.find(InputBase);
+	input.simulate("keyPress", { key: "Enter" });
+	expect(actionFunc.mock.calls.length).toBe(1);
+
+	input.simulate("keyPress", { key: "a" });
+	expect(actionFunc.mock.calls.length).toBe(1);
+});
+
+it("Triggers action function when send button is pressed", () => {
+	const actionFunc = jest.fn();
+	const wrapper = shallow(
+		<TextBox
+			onChange={() => {
+				return;
+			}}
+			textbox="textboxValue"
+			sendAction={actionFunc}
+		/>
+	);
+	const button = wrapper.find(IconButton);
+	button.simulate("click");
+	expect(actionFunc.mock.calls.length).toBe(1);
+});
+
+it("Correctly sets and changes the textbox value", () => {
+	const setFunc = jest.fn();
+	const wrapper = shallow(<TextBox textbox="testVal" setTextbox={setFunc} />);
+	const input = wrapper.find(InputBase);
+
+	expect(input.get(0).props.value).toBe("testVal");
+
+	input.simulate("change", { target: { value: "Hello" } });
+	expect(setFunc.mock.calls[0][0]).toBe("Hello");
+});
